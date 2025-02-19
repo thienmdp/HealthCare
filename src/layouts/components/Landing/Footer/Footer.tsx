75aa84950976cc5e191f4b66353cd5807959c080
neight_dev <thienmai1312@gmail.com>
2025-02-19 14:12:22 +0700

    feat: init project

---
+++ src/layouts/components/Landing/Footer/Footer.tsx
@@ -0,0 +1,90 @@
+import path from '@/constants/path'
+import { Globe, Mail, Phone } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer className='w-full'>
+      <div className='px-4 mx-auto max-w-7xl'>
+        <div className='grid max-w-md grid-cols-1 gap-3 py-10 mx-auto sm:grid-cols-3 lg:grid-cols-4 md:gap-12 sm:max-w-3xl lg:max-w-full'>
+          <div className='mb-10 col-span-full lg:col-span-2 lg:mb-0'>
+            <div>
+              <p className='text-2xl font-bold cursor-pointer text-blue_app'>Diagnosis IQ</p>
+              <div className='text-neutral-800'>
+                <p className='mt-8 break-words text-neutral-800'>
+                  <span className='font-semibold'>Diagnosis IQ </span> Smart Clinical Decision Support System for
+                  Automated Hospital.
+                </p>
+                <p className='flex items-center mt-2'>
+                  <Mail className='w-4 mr-2' />
+                  mail@gmail.com
+                </p>
+                <p className='flex items-center '>
+                  <Phone className='w-4 mr-2' />
+                  0977 777 777
+                </p>
+                <Link className='flex items-center ' to='diagnosisiq.com' target='_blank' rel='noopener noreferrer'>
+                  <Globe className='w-4 mr-2' />
+                  diagnosisiq.com
+                </Link>
+                <p className='flex items-center mt-2'>
+                  <span className='font-semibold'>Địa chỉ:</span> 123 Nguyễn Văn Cừ, phường 6, quận 3, Hồ Chí Minh
+                </p>
+              </div>
+            </div>
+          </div>
+          {/*End Col*/}
+          <div className='pr-6 text-left lg:mx-auto min-w-[250px]'>
+            <div>
+              <h3 className='mb-4 font-medium tracking-wide uppercase dark:text-gray-900'>Chính sách & Hướng dẫn</h3>
+              <ul className='space-y-1'>
+                <li>
+                  <a rel='noopener noreferrer' className='text-sm' href={path.landing}>
+                    Hướng dẫn thanh toán VNPAY-QR
+                  </a>
+                </li>
+
+                <li>
+                  <a rel='noopener noreferrer' className='text-sm' href={path.landing}>
+                    Chính sách bảo mật thông tin
+                  </a>
+                </li>
+                <li>
+                  <a rel='noopener noreferrer' className='text-sm' href={path.landing}>
+                    Chính sách vận chuyển, giao nhận
+                  </a>
+                </li>
+                <li>
+                  <a rel='noopener noreferrer' className='text-sm' href={path.landing}>
+                    Chính sách đổi trả, hoàn tiền
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/*End Col*/}
+          <div className='pl-0 lg:pl-6 text-left lg:mx-auto min-w-[220px]'>
+            <div>
+              <h3 className='mb-4 font-medium tracking-wide uppercase dark:text-gray-900'>Về chúng tôi</h3>
+              <ul className='space-y-1'>
+                <li>
+                  <a rel='noopener noreferrer' className='text-sm' href={path.landing}>
+                    Đội ngũ giảng viên
+                  </a>
+                </li>
+                <li>
+                  <a rel='noopener noreferrer' className='text-sm' href={path.landing}>
+                    Liên hệ với chúng tôi
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className='py-6 text-center text-gray-700'>© 2025 diagnosisiq.com - Việt Nam. All Rights Reserved.</div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/layouts/components/Landing/Header/Header.tsx
@@ -0,0 +1,109 @@
+import { useState, useEffect, useCallback } from 'react'
+import { MobileMenu } from './MobileMenu'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { animateScroll as scroll } from 'react-scroll'
+import { useAppSelector } from '@/redux/store'
+import CardUser from '../../CardUser'
+import path from '@/constants/path'
+import { ModalSearch } from '../../Modal'
+import SearchHeader from './SearchHeader'
+import { useTranslation } from 'react-i18next'
+import { ModeToggleI18n } from '@/components/Global/mode-toggle-i18'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+
+const Container = styled.div`
+  .ant-drawer .antd-drawer-content-wrapper .antd-drawer-content .ant-drawer-body {
+    padding: 0px !important;
+  }
+`
+
+export const Header = () => {
+  const { i18n, t } = useTranslation('landing')
+  const [scrollActive, setScrollActive] = useState(false)
+  const user = useAppSelector((auth) => auth.authState.user)
+
+  useEffect(() => {
+    localStorage.setItem('preferredTheme', 'light')
+  }, [])
+
+  const handleScroll = useCallback(() => {
+    setScrollActive(window.scrollY > 20)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
+
+  const handleScrollToTop = useCallback(() => {
+    scroll.scrollToTop({ duration: 300, smooth: true })
+  }, [])
+
+  return (
+    <header
+      className={
+        'fixed top-0 z-50 shadow-md w-full border-b-1 transition-all duration-250 ease-out' +
+        (scrollActive ? ' pt-0 shadow-sm' : '')
+      }
+    >
+      <nav className='bg-white border-gray-200'>
+        <Container className='flex items-center justify-between px-6 py-4 mx-auto containerr max-w-screen-xl min-h-[70px]'>
+          <div className='flex items-center justify-between w-full md:justify-start'>
+            <MobileMenu />
+            <Link
+              to={path.landing}
+              aria-label='logo_home'
+              className='flex items-center text-2xl font-bold cursor-pointer text-blue_app'
+              onClick={() => handleScrollToTop}
+            >
+              Diagnosis IQ
+            </Link>
+            <ModalSearch />
+            {/* <SearchHeader /> */}
+          </div>
+          <div className='flex items-center justify-end text-sm'>
+            <ModeToggleI18n />
+            <ul className='hidden ml-4 w-max md:flex'>
+              <li>
+                <Link
+                  className='mr-4 text-gray-700 cursor-pointer hover:text-blue_app min-w-fit'
+                  to={path.about}
+                  onClick={() => handleScrollToTop}
+                  aria-current='page'
+                >
+                  {t('header.about')}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className='mr-4 text-gray-700 cursor-pointer hover:text-blue_app min-w-fit'
+                  to={'#contact'}
+                  onClick={() => handleScrollToTop}
+                  aria-current='page'
+                >
+                  {t('header.contact')}
+                </Link>
+              </li>
+            </ul>
+            <span className='hidden w-max md:block'>
+              {user && <CardUser />}
+              {!user && (
+                <div className='flex items-center w-max'>
+                  <Link
+                    className={cn(buttonVariants({ variant: 'gooeyLeft' }), '!rounded-full !px-3 !py-2 min-w-[98px]')}
+                    to={path.signin}
+                    onClick={() => handleScrollToTop}
+                  >
+                    {t('header.login')}
+                  </Link>
+                </div>
+              )}
+            </span>
+          </div>
+        </Container>
+      </nav>
+    </header>
+  )
+}
